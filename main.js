6d6a090c8f4cf91811bb33d24adac9548cc3bf4a
Felix Wich <fx.wich@outlook.de>
2024-11-26 20:24:20 +0100

    Pages hinzugefügt und in main eingebunden

---
+++ main.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import UeberUns from "./pages/UeberUns"
+import UnsereGeschichte from "./pages/UnsereGeschichte"
+
+
+
+export default function Main(){
+    return (
+    <div>
+
+
+    </div>
+
+    )
+
+}
