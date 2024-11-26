525726f91c4dc92771b8b416cc0d5ea708368b31
Felix Wich <fx.wich@outlook.de>
2024-11-26 20:49:17 +0100

    Index hinzugefügt

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home"
-import UeberUns from "./pages/UeberUns"
-import UnsereGeschichte from "./pages/UnsereGeschichte"
+import Home from "./pages/Home";
+import UeberUns from "./pages/UeberUns";
+import UnsereGeschichte from "./pages/UnsereGeschichte";
 
-
-
-export default function Main(){
-    return (
+export default function Main() {
+  return (
     <div>
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/UeberUns" element={<UeberUns />} />
+          <Route path="/UnsereGeschichte" element={<UnsereGeschichte />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
-    )
-
+  );
 }
